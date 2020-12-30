c64fe66a9cb8c0dbc4dc5aff3cf7ae65e1515109
Žan V. Dragan <zan.v.dragan@progmbh.com>
2020-12-30 17:12:54 +0100

    [ignore] Move category handlers in front (alphabet).

--- app/main.go
+++ app/main.go
@@ -25,6 +25,12 @@ func main() {
 
 	http.Handle("/", appHandler{Database: db, Handler: dashboard.Handler})
 
+	http.Handle(category.BaseUrl, appHandler{Database: db, Handler: category.ListHandler})
+	http.Handle(category.BaseUrl+"add/", appHandler{Database: db, Handler: category.AddHandler})
+	http.Handle(category.BaseUrl+"delete/", appHandler{Database: db, Handler: category.DeleteHandler})
+	http.Handle(category.BaseUrl+"edit/", appHandler{Database: db, Handler: category.EditHandler})
+	http.Handle(category.BaseUrl+"view/", appHandler{Database: db, Handler: category.ViewHandler})
+
 	http.Handle(transaction.BaseUrl, appHandler{Database: db, Handler: transaction.ListHandler})
 	http.Handle(transaction.BaseUrl+"add/", appHandler{Database: db, Handler: transaction.AddHandler})
 	http.Handle(transaction.BaseUrl+"delete/", appHandler{Database: db, Handler: transaction.DeleteHandler})
@@ -39,12 +45,6 @@ func main() {
 		transaction_template.BaseUrl+"edit/", appHandler{Database: db, Handler: transaction_template.EditHandler})
 	http.Handle(
 		transaction_template.BaseUrl+"view/", appHandler{Database: db, Handler: transaction_template.ViewHandler})
-
-	http.Handle(category.BaseUrl, appHandler{Database: db, Handler: category.ListHandler})
-	http.Handle(category.BaseUrl+"add/", appHandler{Database: db, Handler: category.AddHandler})
-	http.Handle(category.BaseUrl+"delete/", appHandler{Database: db, Handler: category.DeleteHandler})
-	http.Handle(category.BaseUrl+"edit/", appHandler{Database: db, Handler: category.EditHandler})
-	http.Handle(category.BaseUrl+"view/", appHandler{Database: db, Handler: category.ViewHandler})
 
 	if err = http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalln(err.Error())
